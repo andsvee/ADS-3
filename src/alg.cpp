5c72b3e573d9e874e3b5a6570c05d19720b04736
andsvee <160344384+andsvee@users.noreply.github.com>
2024-03-22 23:28:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,28 +9,27 @@ int cbinsearch(int *arr, int size, int value) {
 
     while (left <= right) {
         int mid = left + (right - left) / 2;
-
         if (arr[mid] == value) {
             count++;
-            int temp = mid - 1;
-            while (temp >= 0 && arr[temp] == value) {
+            int i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
                 count++;
-                temp--;
+                i--;
             }
-            temp = mid + 1;
-            while (temp < size && arr[temp] == value) {
+            i = mid + 1;
+            while (i < size && arr[i] == value) {
                 count++;
-                temp++;
+                i++;
             }
             return count;
-        }
-        else if (arr[mid] < value) {
+        } else if (arr[mid] < value) {
             left = mid + 1;
-        }
-        else {
+        } else {
             right = mid - 1;
         }
     }
-
+    if (count == 0) {
+        return 0;
+    }
     return count;
 }
